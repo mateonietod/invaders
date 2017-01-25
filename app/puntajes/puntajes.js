a2b70e72e6874d8706c92daa7be6de537447f9f4
Julian Izquierdo <julian.izquierdo@mocionsoft.com>
2017-01-25 13:28:32 -0500

    Ajuste de redirección.

--- app/puntajes/puntajes.js
+++ app/puntajes/puntajes.js
@@ -4,9 +4,6 @@ angular.module('app').controller("PuntajesCtrl", function ($scope, $state, _moci
     ctrl.user = $user;
     ctrl.scores = {};
 
-    if(!$user.id)
-        return $state.go("init");
-
     _mocifire.database().ref('usuarios').orderByChild('score').limitToLast(9).once('value', function (snapshot) {
         ctrl.scores = snapshot.val();
         $scope.$apply();
