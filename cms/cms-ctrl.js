8801881286d53a878265ee5ec3d07a7d3fb1f36e
Mateo Nieto <mateo.nieto@mocionsoft.com>
2017-02-15 08:56:56 -0500

    Change to new values in databse

--- cms/cms-ctrl.js
+++ cms/cms-ctrl.js
@@ -1,70 +1,55 @@
-var app = angular.module('app', []).controller('cmsCtrl', function ($scope) {
-
-
+var app = angular.module('app', []).controller('cmsCtrl', function($scope) {
 
     $scope.totalUsuarios = 0;
-    $scope.totalIntentos = 0;   
+    $scope.totalIntentos = 0;
+    $scope.totalDerrotados = 0;
     $scope.usuarios = [];
     $scope.form = {};
     $scope.cargando = true;
     $scope.logged = false;
 
-
-  
-    
-    
-
-/**
- * 
- * USUARIOS
- * 
- */
+    /**
+     *
+     * USUARIOS
+     *
+     */
 
     //Verifica si el usuario ya está logueado
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-       $scope.logged = true;
-      console.log('logged');
-        firebase.database().ref('usuarios').once('value', function (snap) {
-            console.log("snap ",snap);
-            angular.forEach(snap.val(), function(element) {
-                console.log("contando");
-                
-                $scope.totalIntentos += (element.juegos) ? element.juegos : 0;
-                $scope.totalUsuarios++;
-            $scope.usuarios.push(element); 
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            $scope.logged = true;
+            console.log('logged');
+            firebase.database().ref('scores').once('value', function(snap) {
+                console.log("snap ", snap);
+                angular.forEach(snap.val(), function(element) {
+                    console.log("contando");
+                    $scope.totalIntentos += (element.juegos) ? element.juegos : 0;
+                    $scope.totalDerrotados += (element.sz) ? element.sz : 0;
+                    $scope.totalUsuarios++;
+                    $scope.usuarios.push(element);
+                });
+                $scope.cargando = false;
+                if ($scope.$root && $scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest')
+                    $scope.$apply();
             });
-
-            $scope.cargando = false;
-            if ($scope.$root && $scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest')
-            $scope.$apply();
-        });
-  } else {
-    // No user is signed in.
-  }
-});
-
-
-$scope.login = function(){
-    console.log ("mail ", $scope.form.email);
-    console.log ("pass ", $scope.form.pass);
-    
-    firebase.auth().signInWithEmailAndPassword($scope.form.email, $scope.form.pass).then(function(user){
-        //Todo bien llevelo al cms
-        $scope.logged = true;
-    }).catch(function(error){
-        console.log(error);
+        } else {
+            // No user is signed in.
+        }
     });
 
-}
+    $scope.login = function() {
+        console.log("mail ", $scope.form.email);
+        console.log("pass ", $scope.form.pass);
 
+        firebase.auth().signInWithEmailAndPassword($scope.form.email, $scope.form.pass).then(function(user) {
+            //Todo bien llevelo al cms
+            $scope.logged = true;
+        }).catch(function(error) {
+            console.log("Nope: ",error);
+        });
+    }
 
-
-
-
-
-
-    $scope.exportar = function () {
+    $scope.exportar = function() {
         var blob = new Blob([document.getElementById('table-export').innerHTML], {
             type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
         });
@@ -72,12 +57,10 @@ $scope.login = function(){
         saveAs(blob, "Users" + date + ".xls");
     };
 
-        var prettyDate = function (date, startDate) {
+    var prettyDate = function(date, startDate) {
         var now = new Date();
-        return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear() + " " + now.getHours() + ':'
-        + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now
-            .getSeconds()) : (now.getSeconds())));
+        return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear() + " " + now.getHours() + ':' +
+            ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now
+                .getSeconds()) : (now.getSeconds())));
     }
-
-
 });
